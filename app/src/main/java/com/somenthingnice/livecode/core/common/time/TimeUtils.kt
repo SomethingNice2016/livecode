SomethingNice2016/numbers

create test project

app/src/main/java/com/somenthingnice/testtask/core/common/time/TimeUtils.kt
@@ -1,0 +1,7 @@
+package com.somenthingnice.testtask.core.common.time
+
+interface TimeUtils {
+
+    fun getCurrentTime(): Long
+
+}

app/src/main/java/com/somenthingnice/testtask/core/ui/viewBinding/ActivityViewBindingProperty.kt
@@ -1,0 +1,48 @@
+package com.somenthingnice.testtask.core.ui.viewBinding
+
+import android.app.Activity
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.viewbinding.ViewBinding
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+@Suppress("UNCHECKED_CAST")
+class ActivityViewBindingProperty<T : ViewBinding>(
+    private val activity: AppCompatActivity,
+    private val bindingClass: Class<T>
+) : ReadOnlyProperty<Activity, T>, DefaultLifecycleObserver {
+    private var binding: T? = null
+        get() {
+            if (field == null)
+                field = bindingClass.getMethod("inflate", LayoutInflater::class.java)
+                    .invoke(null, activity.layoutInflater) as T
+            return field
+        }
+
+    private fun requireBinding() = binding
+        ?: throw IllegalStateException("Use viewBinding between onCreate and onDestroy")
+
+    init {
+        activity.lifecycle.addObserver(this)
+    }
+
+
+    override fun onCreate(owner: LifecycleOwner) {
+        activity.setContentView(requireBinding().root)
+        super.onCreate(owner)
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        activity.lifecycle.removeObserver(this)
+        binding = null
+        super.onDestroy(owner)
+    }
+
+    override fun getValue(thisRef: Activity, property: KProperty<*>): T {
+        return requireBinding()
+    }
+
+}

settings.gradle.kts
@@ -1,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "testtask"
+include(":app")

SomethingNice2016/livecode

app/src/main/java/com/somenthingnice/livecode/core/common/time/TimeUtils.kt
@@ -1,0 +1,7 @@
+package com.somenthingnice.livecode.core.common.time
+
+interface TimeUtils {
+
+    fun getCurrentTime(): Long
+
+}

app/src/main/java/com/somenthingnice/livecode/core/ui/viewBinding/ActivityViewBindingProperty.kt
@@ -1,0 +1,48 @@
+package com.somenthingnice.livecode.core.ui.viewBinding
+
+import android.app.Activity
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.viewbinding.ViewBinding
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+@Suppress("UNCHECKED_CAST")
+class ActivityViewBindingProperty<T : ViewBinding>(
+    private val activity: AppCompatActivity,
+    private val bindingClass: Class<T>
+) : ReadOnlyProperty<Activity, T>, DefaultLifecycleObserver {
+    private var binding: T? = null
+        get() {
+            if (field == null)
+                field = bindingClass.getMethod("inflate", LayoutInflater::class.java)
+                    .invoke(null, activity.layoutInflater) as T
+            return field
+        }
+
+    private fun requireBinding() = binding
+        ?: throw IllegalStateException("Use viewBinding between onCreate and onDestroy")
+
+    init {
+        activity.lifecycle.addObserver(this)
+    }
+
+
+    override fun onCreate(owner: LifecycleOwner) {
+        activity.setContentView(requireBinding().root)
+        super.onCreate(owner)
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        activity.lifecycle.removeObserver(this)
+        binding = null
+        super.onDestroy(owner)
+    }
+
+    override fun getValue(thisRef: Activity, property: KProperty<*>): T {
+        return requireBinding()
+    }
+
+}

settings.gradle.kts
@@ -1,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "livecode"
+include(":app")